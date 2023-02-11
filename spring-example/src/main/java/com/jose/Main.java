09b94f13d4c34286c113b45725d064c09aba7a73
guesanjo <guesanjoupv@gmail.com>
2023-02-11 19:28:48 +0100

    feat : json object get

--- spring-example/src/main/java/com/jose/Main.java
+++ spring-example/src/main/java/com/jose/Main.java
@@ -5,24 +5,33 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Objects;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
 public class Main {
-    public static void main(String [] args){
-        SpringApplication.run(Main.class,args);
-    }
-@GetMapping("/")
-    public GreetResponse greet(){
-        //hello is the value of key greet
-        return new GreetResponse("Hello");
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
     }
 
-  //  record GreetResponse (String greet){}
+    @GetMapping("/")
+    public GreetResponse greet() {
+        //hello is the value of key greet
+        GreetResponse response = new GreetResponse("Hello",
+                List.of("Java", "Go"),
+                new Person("Alex")
+        );
+        return response;
+    }
+
+    record Person(String name) {
+    }
+
+    record GreetResponse(String greet, List<String> favLang, Person person) {
+    }
 
     // why we get json response == library jackson
-    class GreetResponse{
+    /*static class GreetResponse{
 //greet key
         private final String greet;
 
@@ -54,4 +63,6 @@ public class Main {
             return Objects.hash(greet);
         }
     }
+*/
+
 }
